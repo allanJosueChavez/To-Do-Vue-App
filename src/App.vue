d92fa32dc9df0626202bc5025227f9656ab3e081
Allan Chávez <allancss2009@hotmail.com>
2022-10-13 09:26:51 -0600

    feat: first commit to-do app

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
+import {ref, onMounted, computed, watch} from 'vue'
+</script>
+
+<template>
+  <div style="text-align:center">
+  <br/>
+        <h4>
+          Welcome to your to-do list app, built with VUE!
+        </h4></div>
+  <div style="float: right">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <ToDoList msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
